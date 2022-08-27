ceebf49aa5092108d270633bef5e1ac5234616ee
kang <yi1115610574@gmail.com>
2022-08-27 12:02:44 +0800

    Improve RandomSeq from source code
    - Source code: https://github.com/kevin-wayne/algs4/blob/800d8809cac01e1b3ac11bf1fbd6f9ca01a331c8/src/main/java/edu/princeton/cs/algs4/RandomSeq.java
    - Replace uniform with uniformDouble from StdRandom
    - When give only one parameter, generate random number is [0.0, 1.0)
    - When input parameter count not equal one or three, will throw IllegalArgumentException

--- src/main/java/kang/section_11model/RandomSeq.java
+++ src/main/java/kang/section_11model/RandomSeq.java
@@ -6,11 +6,22 @@ import algs4.StdRandom;
 public class RandomSeq {
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
-        double lo = Double.parseDouble(args[1]);
-        double hi = Double.parseDouble(args[2]);
-        for (int i = 0; i < N; i++) {
-            double x = StdRandom.uniform(lo, hi);
-            StdOut.printf("%.2f\n", x);
+
+        if (args.length == 1) {
+            for (int i = 0; i < N; i++) {
+                double random = StdRandom.uniformDouble(0.0, 1.0);
+                StdOut.println(random);
+            }
+        } else if (args.length == 3) {
+            double lo = Double.parseDouble(args[1]);
+            double hi = Double.parseDouble(args[2]);
+
+            for (int i = 0; i < N; i++) {
+                double random = StdRandom.uniformDouble(lo, hi);
+                StdOut.printf("%.2f\n", random);
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid number of arguments");
         }
     }
 }

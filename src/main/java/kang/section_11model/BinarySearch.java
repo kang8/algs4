7c4c9b0a7d87d17b4a3e159ea8c484230507f72b
kang <yi1115610574@gmail.com>
2022-08-27 10:50:45 +0800

    Implement BinarySearch and add test data

---
+++ src/main/java/kang/section_11model/BinarySearch.java
@@ -0,0 +1,40 @@
+package kang.section_11model;
+
+import algs4.In;
+import algs4.StdIn;
+import algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] whitelist = new In(args[0]).readAllInts();
+
+        Arrays.sort(whitelist);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (indexOf(whitelist, key) == -1) {
+                StdOut.println(key);
+            }
+        }
+    }
+
+    private static int indexOf(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}

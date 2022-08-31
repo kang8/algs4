26d233856c21400a765d240a0b975ab36736332d
kang <yi1115610574@gmail.com>
2022-08-31 12:39:09 +0800

    Fix 1.1.10 compile-time error

--- src/main/java/kang/section_11model/Ex_1_1_10.java
+++ src/main/java/kang/section_11model/Ex_1_1_10.java
@@ -13,10 +13,7 @@ package kang.section_11model;
  */
 public class Ex_1_1_10 {
     public static void main(String[] args) {
-        /*
-         * 没有初始化数组
-         */
-        int[] a;
+        int[] a = new int[10];
         for (int i = 0; i < 10; i++)
             a[i] = i * i;
     }

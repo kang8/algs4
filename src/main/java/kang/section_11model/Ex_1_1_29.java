f976a2e149dc7ff0c8459cbad9d8ab9b0d239e08
kang <yi1115610574@gmail.com>
2022-09-08 07:59:11 +0800

    Complete Ex_1_1_29

--- src/main/java/kang/section_11model/Ex_1_1_29.java
+++ src/main/java/kang/section_11model/Ex_1_1_29.java
@@ -5,11 +5,11 @@ import algs4.In;
 import java.util.Arrays;
 
 /**
- * Equal keys. Add to {@link BinarySearch} a static method {@link BinarySearch#indexOf(int[], int)}
+ * Equal keys. Add to {@link BinarySearch} a static method {@link #rank(int[], int)}
  * that takes a key and a sorted array of int values (some of which may be equal) as arguments
- * and returns the number of elements that are smaller than the key and a similar method count() that
+ * and returns the number of elements that are smaller than the key and a similar method {@link #count(int[], int)} that
  * return the number of elements equal to the key. Note: If i and j are the values returned by
- * {@link BinarySearch#indexOf(int[], int)} and `count(key, a)` respectively, then a[i..i+j-1] are
+ * {@link #rank(int[], int)} and {@link #count(int[], int)} respectively, then a[i..i+j-1] are
  * the values in the array that are equal to key.
  */
 public class Ex_1_1_29 {
@@ -17,20 +17,25 @@ public class Ex_1_1_29 {
 
     public static void main(String[] args) {
         int[] bases = new In(BASE_FILE).readAllInts();
+        int key = 77;
 
         Arrays.sort(bases);
 
-        System.out.println(findFirstKey(bases, 10));
-        System.out.println(findFirstKey(bases, 23));
-        System.out.println(findFirstKey(bases, 77));
-        System.out.println(findFirstKey(bases, 98));
+        int indexOf = rank(bases, key);
+        int count = count(bases, key);
 
-        System.out.println("-------------------");
+        for (int i = 0; i <= count; i++) {
+            System.out.println(bases[i + indexOf + 1]);
+        }
+    }
 
-        System.out.println(findLastKey(bases, 10));
-        System.out.println(findLastKey(bases, 23));
-        System.out.println(findLastKey(bases, 77));
-        System.out.println(findLastKey(bases, 98));
+    private static int rank(int[] arr, int key) {
+        int indexOfFirstKey = findFirstKey(arr, key);
+        return indexOfFirstKey == 0 ? 0 : indexOfFirstKey - 1;
+    }
+
+    private static int count(int[] arr, int key) {
+        return findLastKey(arr, key) - findFirstKey(arr, key);
     }
 
     private static int findFirstKey(int[] arr, int key) {

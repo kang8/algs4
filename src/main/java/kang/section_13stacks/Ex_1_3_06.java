af79aca8d5c046f4dd8c3d674350b4e74140d99f
kang <yi1115610574@gmail.com>
2022-10-20 13:10:35 +0800

    Compelte Ex_1_3_06

---
+++ src/main/java/kang/section_13stacks/Ex_1_3_06.java
@@ -0,0 +1,17 @@
+package kang.section_13stacks;
+
+/**
+ * What does the following code fragment do to the queue q?
+ * <pre>{@code
+ * Stack<String> stack = new Stack<String>();
+ * while(!q.isEmpty())
+ *      stack.push(q.dequeue());
+ * while(!stack.isEmpty())
+ *      q.enqueue(stack.pop());
+ * }</pre>
+ *
+ * @Answer: reverse queue sequence.
+ * 反转 queue 的序列
+ */
+public class Ex_1_3_06 {
+}

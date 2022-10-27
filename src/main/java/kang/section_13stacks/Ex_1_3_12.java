e53d64ca0a701b7660d8c07a255a2fe35533931b
kang <yi1115610574@gmail.com>
2022-10-27 12:36:18 +0800

    Complete Ex_1_3_13

--- src/main/java/kang/section_13stacks/Ex_1_3_12.java
+++ src/main/java/kang/section_13stacks/Ex_1_3_12.java
@@ -1,10 +1,10 @@
 package kang.section_13stacks;
 
 /**
- * Write an iterable {@link Stack} client that has a static method copy() that takes a stack of
- * strings as argument and returns a copy of the stack. Note: This ability is a prime example of
- * the value of having an iterator, because it allows development of such functionality without
- * changing the basic API.
+ * Write an iterable {@link Stack} client that has a static method {@link Stack#copy(Stack)} that takes a
+ * stack of strings as argument and returns a copy of the stack. Note: This ability is a prime
+ * example of the value of having an iterator, because it allows development of such
+ * functionality without changing the basic API.
  */
 public class Ex_1_3_12 {
     public static void main(String[] args) {

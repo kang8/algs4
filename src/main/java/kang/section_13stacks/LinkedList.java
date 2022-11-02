e00d66f7dfa38caba8010f00173d98f30ac950cf
kang <yi1115610574@gmail.com>
2022-11-02 13:01:13 +0800

    Complete Ex_1_3_24

--- src/main/java/kang/section_13stacks/LinkedList.java
+++ src/main/java/kang/section_13stacks/LinkedList.java
@@ -1,10 +1,10 @@
 package kang.section_13stacks;
 
 public class LinkedList<Item> {
-    public Node first;
+    private Node<Item> first;
 
     public void removeListNode() {
-        Node node = first;
+        Node<Item> node = first;
 
         // Find the second last node.
         while (node.next.next != null) {
@@ -31,7 +31,7 @@ public class LinkedList<Item> {
             return;
         }
 
-        Node previous = first;
+        Node<Item> previous = first;
 
         for (int i = 1; i < k - 1; i++) {
             if (previous.next == null) {
@@ -45,11 +45,11 @@ public class LinkedList<Item> {
             throw new IllegalArgumentException("Can't Delete [" + k + "]th element.");
         }
 
-        previous.next = previous.next.next;
+        removeAfter(previous);
     }
 
     public boolean find(Item i) {
-        Node node = first;
+        Node<Item> node = first;
         while (node != null) {
             if (node.item == i) return true;
             node = node.next;
@@ -58,18 +58,23 @@ public class LinkedList<Item> {
         return false;
     }
 
-    private class Node {
-        Item item;
-        Node next;
+    private void removeAfter(Node<Item> node) {
+        if (node == null || node.next == null) return;
 
-        Node(Item element, Node next) {
+        node.next = node.next.next;
+    }
+
+    private static class Node<Item> {
+        Item item;
+        Node<Item> next;
+
+        Node(Item element, Node<Item> next) {
             this.item = element;
             this.next = next;
         }
 
         Node() {
-            this.item = null;
-            this.next = null;
+            this(null, null);
         }
     }
 
@@ -78,12 +83,12 @@ public class LinkedList<Item> {
     }
 
     public LinkedList(Item[] items) {
-        first = new Node();
-        Node node = first;
+        first = new Node<>();
+        Node<Item> node = first;
 
         for (Item item : items) {
             node.item = item;
-            Node temp = new Node();
+            Node<Item> temp = new Node<>();
 
             node.next = temp;
             node      = temp;
@@ -97,7 +102,7 @@ public class LinkedList<Item> {
 
         if (first == null) return str.toString();
 
-        Node temp = first;
+        Node<Item> temp = first;
         while (temp != null) {
             str.append(temp.item).append(" ");
             temp = temp.next;
